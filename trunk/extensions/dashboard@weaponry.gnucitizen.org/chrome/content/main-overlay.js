41b08ec5932e0f9e1cccac2c54466bfb8212d6f4
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2010-02-07 13:04:58 +0000

    change of trunk

--- trunk/extensions/dashboard@weaponry.gnucitizen.org/chrome/content/main-overlay.js
+++
@@ -1,5 +0,0 @@
-// install load event listener
-window.addEventListener('load', function () {
-	// when the page is loaded open the dashboard
-	window.open_content('chrome://dashboard/content/dashboard.xul');
-}, true);
