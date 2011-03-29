035d2a2308f38aefa32922a5fbfd81682fbd3aa2
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-03-29 23:08:57 +0000

    importing consoles

---
+++ xulrunner/weaponry/distribution/bundles/consoles@weaponry.gnucitizen.org/components/WeaponryConsolesService.js
@@ -0,0 +1,82 @@
+/**
+ *  WeaponryConsolesService.js
+ *  Copyright (C) 2007-2011  GNUCITIZEN
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+const CR = Components.results;
+const CC = Components.classes;
+const CI = Components.interfaces;
+
+/* ------------------------------------------------------------------------ */
+
+const CHROMEBASE = 'consoles.weaponry.gnucitizen.org';
+
+/* ------------------------------------------------------------------------ */
+
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
+/* ------------------------------------------------------------------------ */
+
+function WeaponryConsolesService() {
+	Components.utils.import('resource://common.weaponry.gnucitizen.org/content/mod/weaponryCommon.jsm');
+}
+
+WeaponryConsolesService.prototype = {
+	classDescription: 'Weaponry Consoles Service',
+	classID: Components.ID('{608c7690-558e-11e0-b8af-0800200c9a66}'),
+	contractID: '@consoles.weaponry.gnucitizen.org/service;1',
+	QueryInterface: XPCOMUtils.generateQI([CI.IWeaponryConsolesService]),
+	
+	/* -------------------------------------------------------------------- */
+	
+	get wrappedJSObject () {
+		return this;
+	},
+	
+	/* -------------------------------------------------------------------- */
+	
+	executeCommand: function (console, command, args, handler, context) {
+		let launchers = weaponryCommon.retrieveComponents('weaponry-consoles-command-launchers', 'IWeaponryConsolesCommandLauncher');
+		let launchersLength = launchers.length;
+		
+		let i;
+		let launcher;
+		
+		for (i = 0; i < launchersLength; i += 1) {
+			launcher = launchers[i];
+			
+			if (launcher.supportsConsole(console) && launcher.supportsCommand(command)) {
+				launcher.executeCommand(console, command, args, handler, context);
+				
+				return;
+			}
+		}
+		
+		throw new Error('unsupported command ' + command);
+	}
+};
+
+/* ------------------------------------------------------------------------ */
+
+if (XPCOMUtils.generateNSGetFactory) {
+	var NSGetFactory = XPCOMUtils.generateNSGetFactory([WeaponryConsolesService]);
+} else {
+	var NSGetModule = XPCOMUtils.generateNSGetModule([WeaponryConsolesService]);
+}
+
+/*  GNUCITIZEN (Information Security Think Tank)
+ **********************************************/
