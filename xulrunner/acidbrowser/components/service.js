18d79b10f6a876c5cecfef94c7e74e47a173d072
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-03-30 22:18:48 +0000

    changed names in application.ini, service.js improvements

--- xulrunner/acidbrowser/components/service.js
+++ xulrunner/acidbrowser/components/service.js
@@ -72,6 +72,7 @@ Service.prototype = {
 	
 	initializeComponent: function (subject, topic, data) {
 		weaponryCommon.getService('@browser.weaponry.gnucitizen.org/service;1', 'IWeaponryBrowserService').registerDefaultContentHandlers();
+		weaponryCommon.getService('@viewers.weaponry.gnucitizen.org/service;1', 'IWeaponryViewersService').registerContentViewer('fuck/me', 'about:config');
 	},
 	
 	deinitializeComponent: function (subject, topic, data) {
