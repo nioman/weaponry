313607520a5c5cb0307e6ad8ba489e7a9e0e3948
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-06-17 23:50:45 +0000

    major cross-platform update

---
+++ trunk/xulrunner/weaponry/extensions/development@weaponry.gnucitizen.org/defaults/preferences/development.weaponry.gnucitizen.org.js
@@ -0,0 +1,51 @@
+/**
+ *  development.weaponry.gnucitizen.org.js
+ *  Copyright (C) 2007-2011  GNUCITIZEN
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+// weaponry development preferences
+pref('org.gnucitizen.weaponry.development.warn', true);
+pref('org.gnucitizen.weaponry.development.openErrorConsoleOnStartup', false);
+
+// app update preferences
+pref("app.update.log", true);
+
+// browser dom preferences
+pref('browser.dom.window.dump.enabled', true);
+
+// javascript options preferences
+pref('javascript.options.showInConsole', true);
+pref('javascript.options.strict', true);
+
+// nglayout debug preferences
+pref('nglayout.debug.disable_xul_cache', true);
+pref('nglayout.debug.disable_xul_fastload', true);
+
+// inspector preferences
+pref('inspector.blink.border-color', '#CC0000');
+pref('inspector.blink.border-width', 2);
+pref('inspector.blink.duration', 1200);
+pref('inspector.blink.on', true);
+pref('inspector.blink.speed', 100);
+pref('inspector.blink.invert', false);
+pref('inspector.dom.showAnon', true);
+pref('inspector.dom.showWhitespaceNodes', true);
+pref('inspector.dom.showAccessibleNodes', false);
+pref('inspector.dom.showProcessingInstructions', true);
+
+/*  GNUCITIZEN (Information Security Think Tank)
+ **********************************************/
