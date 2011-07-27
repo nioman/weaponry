3ac1ef93151ae3c72fd3024f0bf7a84290f2983a
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-07-27 07:16:00 +0000

    the main window is now ready for primetime use

--- xulrunner/acidplatform/distribution/bundles/basic@acidplatform.gnucitizen.org/defaults/preferences/basic.acidplatform.gnucitizen.org.js
+++ xulrunner/acidplatform/distribution/bundles/basic@acidplatform.gnucitizen.org/defaults/preferences/basic.acidplatform.gnucitizen.org.js
@@ -17,6 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+// default acidplatform basic preferences
 pref('org.gnucitizen.acidplatform.basic.applications', '[]');
 
 /*  GNUCITIZEN (Information Security Think Tank)
