7ea5a9e36f5d3f32582d3f59974b964646ce3764
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-08-28 23:47:34 +0000

    test case completed

--- branches/crash-000/defaults/preferences/application.js
+++ branches/crash-000/defaults/preferences/application.js
@@ -12,3 +12,6 @@ pref('browser.dom.window.dump.enabled', true);
 // nglayout debug preferences
 pref('nglayout.debug.disable_xul_cache', true);
 pref('nglayout.debug.disable_xul_fastload', true);
+
+// browser preferences
+pref('browser.chromeURL', 'chrome://main/content/main.xul');
