375b27b2b4737a380253c595bbf267e99ab32a82
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-09-05 20:04:45 +0000

    major refactoring of chrome registration names

--- xulrunner/weaponry/distribution/bundles/xulrunner@weaponry.gnucitizen.org/defaults/preferences/org.gnucitizen.weaponry.xulrunner.js
+++ xulrunner/weaponry/distribution/bundles/xulrunner@weaponry.gnucitizen.org/defaults/preferences/org.gnucitizen.weaponry.xulrunner.js
@@ -1,5 +1,5 @@
 /**
- *  xulrunner.weaponry.gnucitizen.org.js
+ *  org.gnucitizen.weaponry.xulrunner.js
  *  Copyright (C) 2007-2011  GNUCITIZEN
  *  
  *  This program is free software; you can redistribute it and/or modify
