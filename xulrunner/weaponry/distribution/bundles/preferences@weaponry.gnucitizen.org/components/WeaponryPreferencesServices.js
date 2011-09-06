79560453c1476ca31146d2921bc01b7f23ffe985
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-09-06 17:46:00 +0000

    general code normalization

--- xulrunner/weaponry/distribution/bundles/preferences@weaponry.gnucitizen.org/components/WeaponryPreferencesServices.js
+++ xulrunner/weaponry/distribution/bundles/preferences@weaponry.gnucitizen.org/components/WeaponryPreferencesServices.js
@@ -88,7 +88,7 @@ WeaponryPreferencesService.prototype = {
 			weaponryCommon.setPref('network.proxy.type', 0);
 		}
 		
-		// TODO: take into consideration types variable
+		// TODO: take into consideration "types" variable
 		weaponryCommon.setPref('network.proxy.share_proxy_settings', true);
 		//
 		
