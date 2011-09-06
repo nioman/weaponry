faed5e7b39b6e0483a016c43f15e2989f798f091
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-09-06 17:44:02 +0000

    general code normalization

--- xulrunner/weaponry/extensions/development@weaponry.gnucitizen.org/chrome/content/ovl/toolsMenu.js
+++ xulrunner/weaponry/extensions/development@weaponry.gnucitizen.org/chrome/content/ovl/toolsMenu.js
@@ -17,29 +17,48 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-function handleReloadUICommandEvent(event) {
-	weaponryCommon.reloadAllWindows();
-}
-
-function handleMemoryCommandEvent(event) {
-	weaponryCommon.openWindow(null, 'about:memory?verbose', '', 'all,chrome,resizable,centerscreen,width=800,height=600');
-}
-
-function handleColorsCommandEvent(event) {
-	weaponryCommon.openWindow(null, 'chrome://org.gnucitizen.weaponry.development/content/htm/colors.htm', '', 'all,chrome,resizable,centerscreen,width=400,height=600');
-}
-
-function handleRequestsCommandEvent(event) {
-	weaponryCommon.openWindow(null, 'chrome://org.gnucitizen.weaponry.development/content/htm/requests.htm', '', 'all,chrome,resizable,centerscreen,width=600,height=400');
-}
-
-function handleInspectorCommandEvent(event) {
-	weaponryCommon.openWindow(null, 'chrome://inspector/content/inspector.xul', '', 'all,chrome,resizable,centerscreen');
-}
-
-function handleJsshellCommandEvent(event) {
-	weaponryCommon.openWindow(null, 'chrome://jsshell/content/jsshell.xul', '', 'all,chrome,resizable,centerscreen,width=500,height=400,scrollbars=yes');
-}
+installHandler('org.gnucitizen.weaponry.development.toolsMenu', {
+	reloadUi: function () {
+		weaponryCommon.reloadAllWindows();
+	},
+	
+	openMemoryWindow: function () {
+		weaponryCommon.openWindow(null, 'chrome://org.gnucitizen.weaponry.development/content/htm/memory.htm', '', 'all,chrome,resizable,centerscreen,width=400,height=600');
+	},
+	
+	openColorsWindow: function () {
+		weaponryCommon.openWindow(null, 'chrome://org.gnucitizen.weaponry.development/content/htm/colors.htm', '', 'all,chrome,resizable,centerscreen,width=400,height=600');
+	},
+	
+	openRequestsWindow: function () {
+		weaponryCommon.openWindow(null, 'chrome://org.gnucitizen.weaponry.development/content/htm/requests.htm', '', 'all,chrome,resizable,centerscreen,width=600,height=400');
+	},
+	
+	openInspectorWindow: function () {
+		weaponryCommon.openWindow(null, 'chrome://inspector/content/inspector.xul', '', 'all,chrome,resizable,centerscreen');
+	},
+	
+	openJsshellWindow: function () {
+		weaponryCommon.openWindow(null, 'chrome://jsshell/content/jsshell.xul', '', 'all,chrome,resizable,centerscreen,width=500,height=400,scrollbars=yes');
+	},
+	
+	onDOMContentLoaded: function (event) {
+		if (event.target != document) {
+			return;
+		}
+		
+		if (document.getElementById('tools-menupopup')) {
+			let self = org.gnucitizen.weaponry.development.toolsMenu;
+			
+			bindHandler('weaponry-development-tools-menu-reload-ui-command', 'command', self.reloadUi);
+			bindHandler('weaponry-development-tools-menu-memory-menuitem', 'command', self.openMemoryWindow);
+			bindHandler('weaponry-development-tools-menu-colors-menuitem', 'command', self.openColorsWindow);
+			bindHandler('weaponry-development-tools-menu-requests-menuitem', 'command', self.openRequestsWindow);
+			bindHandler('weaponry-development-tools-menu-inspector-menuitem', 'command', self.openInspectorWindow);
+			bindHandler('weaponry-development-tools-menu-jsshell-menuitem', 'command', self.openJsshellWindow);
+		}
+	}
+});
 
 /*  GNUCITIZEN (Information Security Think Tank)
  **********************************************/
