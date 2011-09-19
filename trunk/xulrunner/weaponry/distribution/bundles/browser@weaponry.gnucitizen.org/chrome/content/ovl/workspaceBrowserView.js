ebb96f67066232d971f10ff1eba4c3be8333350c
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-09-19 20:00:38 +0000

    general code improvements

--- trunk/xulrunner/weaponry/distribution/bundles/browser@weaponry.gnucitizen.org/chrome/content/ovl/workspaceBrowserView.js
+++ trunk/xulrunner/weaponry/distribution/bundles/browser@weaponry.gnucitizen.org/chrome/content/ovl/workspaceBrowserView.js
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-Components.utils.import('resource://workspaces.weaponry.gnucitizen.org/content/mod/weaponryWorkspaces.jsm');
+ensureModule('resource://workspaces.weaponry.gnucitizen.org/content/mod/weaponryWorkspaces.jsm', 'weaponryWorkspaces');
 
 /* ------------------------------------------------------------------------ */
 
