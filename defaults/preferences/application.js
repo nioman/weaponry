1d1e23c6db515e9a55a82765a0f7faab4cd66b33
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-10-10 21:25:45 +0000

    crash-04

---
+++ defaults/preferences/application.js
@@ -0,0 +1,17 @@
+// toolkit preferences
+pref('toolkit.defaultChromeURI', 'chrome://main/content/main.xul');
+pref('toolkit.singletonWindowType', 'main:main');
+
+// javascript options preferences
+pref('javascript.options.showInConsole', true);
+pref('javascript.options.strict', true);
+
+// browser dom preferences
+pref('browser.dom.window.dump.enabled', true);
+
+// nglayout debug preferences
+pref('nglayout.debug.disable_xul_cache', true);
+pref('nglayout.debug.disable_xul_fastload', true);
+
+// browser hidden window preferences
+pref('browser.hiddenWindowChromeURL', 'chrome://main/content/hidden.xul');
