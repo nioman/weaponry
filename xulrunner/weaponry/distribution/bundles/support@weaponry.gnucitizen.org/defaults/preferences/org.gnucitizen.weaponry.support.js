22a958f27f3ea5f8319ced39fb6d26a03a2070c9
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2011-11-26 23:36:27 +0000

    improved preferences

--- xulrunner/weaponry/distribution/bundles/support@weaponry.gnucitizen.org/defaults/preferences/org.gnucitizen.weaponry.support.js
+++ xulrunner/weaponry/distribution/bundles/support@weaponry.gnucitizen.org/defaults/preferences/org.gnucitizen.weaponry.support.js
@@ -17,34 +17,37 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-// browser preferences
 pref('browser.chromeURL', 'chrome://org.gnucitizen.weaponry.browser/content/xul/browserView.xul');
 
-// app update preferences
-pref('app.update.url', 'https://weaponryupdates.appspot.com/update/app.xml?product=%PRODUCT%&version=%VERSION%&build_id=%BUILD_ID%&build_target=%BUILD_TARGET%&locale=%LOCALE%&channel=%CHANNEL%&os_version=%OS_VERSION%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%');
-pref('app.update.url.manual', 'http://weaponry.gnucitizen.com');
-pref('app.update.url.details', 'http://weaponry.gnucitizen.com');
+/* ------------------------------------------------------------------------ */
 
-// extensions get addons preferences
-pref('extensions.getAddons.maxResults', 15);
+pref('app.update.enabled', true);
+pref('app.update.url', 'https://weaponry.gnucitizen.org/applications/update?locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%');
+pref('app.update.url.manual', 'http://weaponry.gnucitizen.org');
+pref('app.update.url.details', 'http://weaponry.gnucitizen.org');
+
+/* ------------------------------------------------------------------------ */
+
+pref('extensions.blocklist.enabled', false);
 pref('extensions.getAddons.cache.enabled', true);
-pref('extensions.getAddons.get.url', 'https://weaponryupdates.appspot.com/extensions/get'); // TODO: add parameters
-pref('extensions.getAddons.search.url', 'https://weaponryupdates.appspot.com/extensions/search'); // TODO: add parameters
-pref('extensions.getAddons.search.browseURL', 'https://weaponryupdates.appspot.com/extensions/search?term=%TERMS%'); // TODO: add parameters
+pref('extensions.getAddons.get.url', 'https://weaponry.gnucitizen.org/extensions/get?locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%&api_version=%API_VERSION%&ids=%IDS%&time_main=%TIME_MAIN%&time_first_paint=%TIME_FIRST_PAINT%&time_session_restored=%TIME_SESSION_RESTORED%');
+pref('extensions.getAddons.maxResults', 15);
+pref('extensions.getAddons.search.browseURL', 'https://weaponry.gnucitizen.org/extensions/browse?locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%&terms=%TERMS%');
+pref('extensions.getAddons.search.url', 'https://weaponry.gnucitizen.org/extensions/search?locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%&api_version=%API_VERSION%&terms=%TERMS%&max_results=%MAX_RESULTS%');
+pref('extensions.update.url', 'https://weaponry.gnucitizen.org/extensions/update?item_id=%ITEM_ID%&item_version=%ITEM_VERSION%&item_status=%ITEM_STATUS%&app_id=%APP_ID%&app_version=%APP_VERSION%&req_version=%REQ_VERSION%&app_os=%APP_OS%&app_abi=%APP_ABI%&app_locale=%APP_LOCALE%&current_app_version=%CURRENT_APP_VERSION%&update_type=%UPDATE_TYPE%&item_maxappversion=%ITEM_MAXAPPVERSION%');
+pref('extensions.webservice.discoverURL', 'https://weaponry.gnucitizen.org/extensions/discover??locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%');
 
-// extensions blacklist preferences
-pref('extensions.blocklist.url', 'https://weaponryupdates.appspot.com/extensions/blacklist'); // TODO: add parameters
+/* ------------------------------------------------------------------------ */
 
-// extensions discover preferences
-pref('extensions.webservice.discoverURL', 'https://weaponryupdates.appspot.com/extensions/discover'); // TODO: add parameters
+pref('plugins.update.url', 'https://weaponry.gnucitizen.org/plugins/update?locale=%LOCALE%&vendor=%VENDOR%&name=%NAME%&id=%ID%&version=%VERSION%&appbuildid=%APPBUILDID%&platformversion=%PLATFORMVERSION%&platformbuildid=%PLATFORMBUILDID%&app=%APP%&os=%OS%&xpcomabi=%XPCOMABI%&build_target=%BUILD_TARGET%&os_version=%OS_VERSION%&channel=%CHANNEL%&distribution=%DISTRIBUTION%&distribution_version=%DISTRIBUTION_VERSION%');
 
-// extensions update preferences
-pref('extensions.update.url', 'https://weaponryupdates.appspot.com/update/ext.xml?req_version=%REQ_VERSION%&item_id=%ITEM_ID%&item_version=%ITEM_VERSION%&item_maxappversion=%ITEM_MAXAPPVERSION%&item_status=%ITEM_STATUS%&app_id=%APP_ID%&app_version=%APP_VERSION%&app_os=%APP_OS%&app_abi=%APP_ABI%&app_locale=%APP_LOCALE%&current_app_version=%CURRENT_APP_VERSION%&update_type=%UPDATE_TYPE%');
-pref('extensions.update.url.manual', 'http://weaponry.gnucitizen.org');
-pref('extensions.update.url.details', 'http://weaponry.gnucitizen.org');
+/* ------------------------------------------------------------------------ */
 
-// weaponry security CertOverrideService preferences
-pref('org.gnucitizen.weaponry.security.CertOverrideService.exceptions.add.3', 'weaponryupdates.appspot.com:443');
+pref('org.gnucitizen.weaponry.common.brandingAboutURI', 'chrome://org.gnucitizen.websecurify.support/content/xul/aboutWindow.xul');
+
+/* ------------------------------------------------------------------------ */
+
+pref('org.gnucitizen.weaponry.security.CertOverrideService.exceptions.add.3', 'weaponry.gnucitizen.org:443');
 pref('org.gnucitizen.weaponry.security.CertOverrideService.exceptions.add.4', 'www.google.com:443');
 
 /*  GNUCITIZEN (Information Security Think Tank)
