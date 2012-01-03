63511129b69bd93fded18f6e692b083993d08c78
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2012-01-03 22:25:39 +0000

    CHROMEBASE const remove from all sources

--- trunk/xulrunner/weaponry/distribution/bundles/security@weaponry.gnucitizen.org/components/WeaponryCertOverrideService.js
+++ trunk/xulrunner/weaponry/distribution/bundles/security@weaponry.gnucitizen.org/components/WeaponryCertOverrideService.js
@@ -23,10 +23,6 @@ const CI = Components.interfaces;
 
 /* ------------------------------------------------------------------------ */
 
-const CHROMEBASE = 'org.gnucitizen.weaponry.security';
-
-/* ------------------------------------------------------------------------ */
-
 Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
 
 /* ------------------------------------------------------------------------ */
