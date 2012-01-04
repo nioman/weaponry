bc08a0110fa4642112c5418c751fbf2c5b84f47e
pdp.gnucitizen <pdp.gnucitizen@991cf3ac-13c0-11de-a4db-e31d5fa7c4f0>
2012-01-04 19:56:19 +0000

    fixed a bug where the file quit command is not registered properly

--- trunk/xulrunner/weaponry/distribution/bundles/common@weaponry.gnucitizen.org/chrome/content/ovl/commonMenu.js
+++ trunk/xulrunner/weaponry/distribution/bundles/common@weaponry.gnucitizen.org/chrome/content/ovl/commonMenu.js
@@ -31,16 +31,21 @@ installHandler('org.gnucitizen.weaponry.common.commonMenu', {
 			return;
 		}
 		
+		let self = org.gnucitizen.weaponry.common.commonMenu;
+		
 		if (document.getElementById('common-commandset')) {
-			let self = org.gnucitizen.weaponry.common.commonMenu;
-			
 			bindHandler('file-menu-quit-command', 'command', self.quit);
 			bindHandler('help-menu-about-command', 'command', self.about);
-			
-			let $stringbundle = document.getElementById('mac-menu-stringbundle');
-			let name = weaponryCommon.xulAppInfo.name;
-			
+		}
+		
+		let $stringbundle = document.getElementById('mac-menu-stringbundle');
+		let name = weaponryCommon.xulAppInfo.name;
+		
+		if (document.getElementById('file-menupopup')) {
 			document.getElementById('file-menu-quit-menuitem').setAttribute('label', $stringbundle.getFormattedString('quit-label', [name]));
+		}
+		
+		if (document.getElementById('help-menupopup')) {
 			document.getElementById('help-menu-about-menuitem').setAttribute('label', $stringbundle.getFormattedString('about-label', [name]));
 		}
 	}
